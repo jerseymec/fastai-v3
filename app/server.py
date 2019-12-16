dcfd1ee42be68545a8b637183e4a1fd6557fea76
Charles <meldoncharles@hotmail.com>
2019-12-15 21:00:17 -0500

    updated the model and url using google drive url generator https://course.fast.ai/deployment_render.html

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1qy5PUTW1vfJ6JueZ_P_NBIQN8lko2PWa'
 export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
